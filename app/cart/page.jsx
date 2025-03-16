51b4a7165d27bbdf5afd57988d8b49dab971af36
vpa <voan0773@gmail.com>
2025-03-16 15:32:41 +0700

    update

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -17,7 +17,7 @@ const Cart = () => {
         <div className="flex-1">
           <div className="flex items-center justify-between mb-8 border-b border-gray-500/30 pb-6">
             <p className="text-2xl md:text-3xl text-gray-500">
-              Your <span className="font-medium text-orange-600">Cart</span>
+              Yours <span className="font-medium text-orange-600">Cart</span>
             </p>
             <p className="text-lg md:text-xl text-gray-500/80">{getCartCount()} Items</p>
           </div>
