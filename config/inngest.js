6b54c568d612043a2ff0d1755d559d313c6742ab
VPA2103 <150929603+VPA2103@users.noreply.github.com>
2025-03-18 12:38:27 +0700

    code

--- config/inngest.js
+++ config/inngest.js
@@ -1,5 +1,6 @@
 import { Inngest } from "inngest";
 import connectDB from "./db";
+import User from "../models/User";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "quickcart-next" });
@@ -13,23 +14,48 @@ export const syncUserCreation = inngest.createFunction(
         event:'clerk/user.created'
     },
 
-    async ({event}) =>{
-        const {id,first_name,last_name,email_address,image_url} = event.data
+    // async ({event}) =>{
+    //     const {id,first_name,last_name,email_address,image_url} = event.data
+
+    //     const userData = {
+    //         _id:id,
+    //         email: Array.isArray(email_address)?email_address[0]:email_address,
+    //         name:first_name + ' ' + last_name,
+    //         imageUrl: image_url,
+
+    //     }
+    //     await connectDB()
+    //     await User.create(userData)
+
+    // }
+    async ({ event }) => {
+        console.log("Received event data:", event.data);
+
+        const { id, first_name, last_name, email_address, image_url } = event.data;
 
         const userData = {
-            _id:id,
-            email: Array.isArray(email_address)?email_address[0]:email_address,
-            name:first_name + ' ' + last_name,
+            _id: String(id),
+            email: Array.isArray(email_address) && email_address.length > 0 ? email_address[0] : "",
+            name: `${first_name} ${last_name}`,
             imageUrl: image_url,
+        };
 
+        try {
+            await connectDB();
+            console.log("Connected to MongoDB!");
+
+            await User.findOneAndUpdate(
+                { _id: id }, 
+                userData, 
+                { upsert: true, new: true }
+            );
+
+            console.log("User created/updated successfully:", userData);
+        } catch (error) {
+            console.error("Error creating user:", error);
         }
-        await connectDB()
-        await User.create(userData)
-
     }
-
 )
-
 
 //inngest function to update user in a database
 
